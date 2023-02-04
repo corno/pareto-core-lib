7f326ea2f07fb655f3b49580a1e9202bad8f4f84
corno <corno.astn.a@schraverus.com>
2023-02-04 14:38:39 +0100

    .

--- pub/src/public/assertUnreachable.ts
+++ pub/src/public/assertUnreachable.ts
@@ -1,6 +1,6 @@
 
 /**
- * au means 'assert unreachable'. Used in the 'default' clause of while statements to ensure that
+ * au means 'assert unreachable'. Used in the 'default' clause of switch statements to ensure that
  * all possible cases have been handled
  * @param _x 
  */
